7449de02ffda2ac69c94cc8e600ccccc9f9fb2df
Rebeca Ruiz <rebeca.ruiz.guardia0102@gmail.com>
2024-10-14 23:22:33 -0500

    Implement js code for button sound

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-alert('Hola mundo');
+function playSonido (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#sonido_${instrumento}`;
+    tecla.onclick = function () {
+        playSonido(idAudio);
+    };
+}
+
+// let contador = 0;
+// while (contador < 9) {
+//     const tecla = listaDeTeclas[contador];
+//     const instrumento = tecla.classList[1];
+//     const idAudio = `#sonido_${instrumento}`;
+
+//     tecla.onclick = function () {
+//         playSonido(idAudio);
+//     };
+//     contador += 1;
+// }
